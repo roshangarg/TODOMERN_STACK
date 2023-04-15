49507393e43977962561dd6de3a56957deed721d
roshansrchout <roshan@srchout.com>
2023-04-15 14:19:01 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
+const path = require('path')
 
 // Init App 
 
@@ -21,12 +22,17 @@ app.use((req , res , next) => {
 app.use('/api/workouts',workoutRoutes)
 app.use('/api/user',userRoutes)
 
+app.use(express.static(path.join(__dirname , '../my_todo_mern_app/build')))
 
+app.get("*" , (req , res ) => {
+    res.sendFile(path.join(__dirname,"../my_todo_mern_app/build/index.html"))
+})
+const port = process.env.PORT || 4000
 // Connect to db 
 mongoose.connect(process.env.MONGO_URI)
         .then(() => {
             // listen for request
-            app.listen(process.env.PORT,() => {
+            app.listen(port,() => {
                 console.log("Connected to the mongoose database  request on port number 4000")
             
             })
